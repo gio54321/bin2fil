67200cbc51447dc55964ad0498704842c542b111
gio54321 <g.dellimmagine@gmail.com>
2018-07-21 17:11:33 +0200

    add auto mode

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var app = require('electron').app
 var BrowserWindow = require('electron').BrowserWindow;
 
+global.sharedObject = {prop1: process.argv}
+
 app.on("ready", function(){
   var mainWindow = new BrowserWindow({
     width:1050,
